306692fcdb0ddccb76c88fa1e9ce7ffaeb20a7f8
Dream-Drive-Project <114352188+Dream-Drive-Project@users.noreply.github.com>
2022-12-13 17:21:22 -0500

    Update connect.php

--- public/connect.php
+++ public/connect.php
@@ -6,7 +6,7 @@
 $dbUsername ="R3zOnZCcuB";
 $dbPassword="JMYUdEV9mY";
 $dbname ="R3zOnZCcuB";
-$conn = new mysqli('remotemysql.com:3306',$dbUsername,$dbPassword,$dbname,3306);
+$conn = new mysqli('remotemysql.com:3306',$dbUsername,$dbPassword,$dbname);
 
 
 if ($conn->connect_error) {
